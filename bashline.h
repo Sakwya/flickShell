353603ffe7432d148a3d20a58014a54ef6b43628
MarcoLu <154767894+MarcoLu922@users.noreply.github.com>
2024-06-05 19:45:29 +0800

    重写

--- bashline.h
+++
@@ -1,7 +0,0 @@
-#include<line.h>
-#include<unistd.h>
-#include<pwd.h>
-#include<fstream>
-extern string home_dir;
-extern void init_shell(void);
-extern string get_command_prompt(void);
--- config.h
+++
@@ -1,4 +0,0 @@
-#define SHOW_PANIC true
-#define SHOW_WAIT_PANIC false
-#define CHAR_BUF_SIZE 1024
-#define MAX_ARGV_LEN 128
